c73cf5d022ac87badeca3bf3e20f1582565cf109
Malhar Kajale <malharkajale@gmail.com>
2022-12-11 12:38:25 +0530

    add deleting todo functionality

--- frontend/src/components/Todo.js
+++ frontend/src/components/Todo.js
@@ -2,12 +2,29 @@
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable react/prop-types */
+
+import useTodoContext from '../hooks/todoReducer';
+
 function Todo({ todo }) {
+  const { dispatch } = useTodoContext();
+
+  async function handleClick() {
+    const request = await fetch(`/todolist/${todo._id}`, {
+      method: 'delete',
+    });
+    const json = await request.json();
+    // todo object can also be used as payload
+    if (request.ok) {
+      dispatch({ type: 'DELETE_TODO', payload: json });
+    }
+  }
+
   return (
     <div className="todo" key={todo._id}>
       <div className="todo-prop">{todo.name}</div>
       <div className="todo-prop">{todo.desc}</div>
       <div className="todo-prop">{todo.createdAt}</div>
+      <input type="button" value="Delete" onClick={handleClick} />
     </div>
   );
 }
